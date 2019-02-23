b679086dc415cefb7ce929c6f89d0e2e3c2808d2
mayankdharwal <mayankdharwal1986@gmail.com>
2019-02-23 10:06:26 +0100

    fix check style warning for package info part 4

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   /**
-   * @param someone
+   * @param someone name of the string
    * @return greeting string
    */
   public final String greet(final String someone) {
